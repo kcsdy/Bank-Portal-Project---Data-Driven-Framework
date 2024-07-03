1882fff321c6503df36643beaf1d1bfe45401d31
kcsdy <chakrabortykuntal27@gmail.com>
2024-07-04 00:15:13 +0100

    Adding new scenarios in withdrawl test

--- src/test/java/testcases/CustomerWithdrawlTest.java
+++ src/test/java/testcases/CustomerWithdrawlTest.java
@@ -3,6 +3,7 @@ package testcases;
 import java.util.Hashtable;
 
 import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -20,5 +21,25 @@ public class CustomerWithdrawlTest extends TestBase{
 		driver.findElement(By.xpath(ObjectReference.getProperty("inputField"))).sendKeys(Data.get("withdrawlAmount"));
 		driver.findElement(By.xpath(ObjectReference.getProperty("submitBtn"))).submit();
 		
+	}
+	
+	@Test(dataProviderClass = TestUtil.class , dataProvider = "dp")
+	public void withdrawlAmtMoreThanBalTest(Hashtable<String,String> data) throws InterruptedException
+	{
+		driver.findElement(By.xpath(ObjectReference.getProperty("inputField"))).sendKeys(data.get("withdrawlAmount"));
+		driver.findElement(By.xpath(ObjectReference.getProperty("submitBtn"))).submit();
+		String errorMsgInPortal = driver.findElement(By.xpath(ObjectReference.getProperty("withdrawlAmtMoreThanBalText"))).getText();
+		Assert.assertTrue(errorMsgInPortal.equals(data.get("errorMsg")));
+		Thread.sleep(3000);
+	}
+	
+	@Test
+	public void withdrawlAmtEqualToBalTest() {
+		String accBal = driver.findElement(By.xpath(ObjectReference.getProperty("balanceValue"))).getText();
+		System.out.println(accBal);
+		driver.findElement(By.xpath(ObjectReference.getProperty("inputField"))).sendKeys(accBal);
+		driver.findElement(By.xpath(ObjectReference.getProperty("submitBtn"))).submit();
+		String successMsg = driver.findElement(By.xpath(ObjectReference.getProperty("withdrawlAmtMoreThanBalText"))).getText();
+		Assert.assertTrue(successMsg.equals("Transaction successful"));
 	}
 }
