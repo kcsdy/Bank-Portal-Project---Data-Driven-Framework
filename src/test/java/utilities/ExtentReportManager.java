bbe61000da325b0ad8c30737c30404d0e07f1459
kcsdy <chakrabortykuntal27@gmail.com>
2025-03-11 14:54:51 +0000

    Added ExtentReportManager file

--- src/test/java/utilities/ExtentReportManager.java
+++ src/test/java/utilities/ExtentReportManager.java
@@ -1,5 +1,17 @@
 package utilities;
 
-public class ExtentReportManager {
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
+public class ExtentReportManager {
+	
+	private ExtentReports extent;
+	
+	public ExtentReports getInstance() {
+		
+		ExtentSparkReporter spark = new ExtentSparkReporter("C:\\Users\\ASUS\\git\\Bank-Portal-Project---Data-Driven-Framework\\src\\test\\resources\\reports\\ExtentReports.html");
+		extent = new ExtentReports();
+		extent.attachReporter(spark);
+		return extent;
+	}
 }
